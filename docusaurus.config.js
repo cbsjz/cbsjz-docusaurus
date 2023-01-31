21182863fb94a0115fee49b0117ec3fd90f6b129
cbsjz <cbsjz@qq.com>
2023-01-31 20:33:59 +0800

    ID cbsjz => shaotori

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'cbsjz',
+  title: 'shaotori',
   tagline: '爱睡觉的cb烧鸡中zzZ',
   url: 'https://cbsjz.org',
   baseUrl: '/',
