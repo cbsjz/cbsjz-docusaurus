4f5733bf8890db092a747a43037ec889d86e45e5
shaotori <cbsjz@qq.com>
2023-07-18 16:11:09 +0800

    wxchat

---
+++ src/pages/wxchat.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx('hero', styles.headerBox)}>
+      <div>
+        <img src="./img/wxchat.jpg" alt="wxchat"/>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      // title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      {/* <main>
+        <HomepageFeatures />
+      </main> */}
+    </Layout>
+  );
+}
