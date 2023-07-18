600d89711745238c4b5588a649f64f75607fb98f
shaotori <cbsjz@qq.com>
2023-07-18 16:25:59 +0800

    q

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -283,12 +283,12 @@ const config = {
               {
                 label: "eMail",
                 href: "mailto:shaotori@qq.com",
-                to: "/email",
+                to: "email",
               },
               {
                 label: "企业微信",
                 href: "https://work.weixin.qq.com/u/vc0cb7a53158d8df64",
-                to: "/wxchat",
+                to: "wxchat",
               },
             ],
           },
